3f78876d7b72d4bd4bb8f167de38b05956ee0355
christine <christamsnow@gmail.com>
2021-01-24 19:51:44 -0500

    added logger

--- src/main/java/com/dmcthreads/demo/DmcThreadsApplication.java
+++ src/main/java/com/dmcthreads/demo/DmcThreadsApplication.java
@@ -1,13 +1,17 @@
 package com.dmcthreads.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DmcThreadsApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DmcThreadsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DmcThreadsApplication.class, args);
+		LOGGER.info("DmcThreadsApplication started successfully.");
 	}
 
 }
