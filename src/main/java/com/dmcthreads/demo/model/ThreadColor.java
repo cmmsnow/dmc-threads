db599ed66f57e0e898e31fb97bb34456bb689993
christine <christamsnow@gmail.com>
2021-01-26 17:13:31 -0500

    comment out pattern code

--- src/main/java/com/dmcthreads/demo/model/ThreadColor.java
+++ src/main/java/com/dmcthreads/demo/model/ThreadColor.java
@@ -12,17 +12,17 @@ public class ThreadColor {
     private String colorName;
     private String hexadecimal;
     private String colorFamily;
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "pattern_id", nullable = false)
-    private ColorPattern colorPattern;
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+////    @JoinColumn(name = "pattern_id", nullable = false)
+//    private ColorPattern colorPattern;
 
     public ThreadColor(){}
 
-    public ThreadColor(String colorName, String hexadecimal, String colorFamily, ColorPattern colorPattern){
+    public ThreadColor(String colorName, String hexadecimal, String colorFamily){
         this.colorName = colorName;
         this.hexadecimal = hexadecimal;
         this.colorFamily = colorFamily;
-        this.colorPattern = colorPattern;
+//        this.colorPattern = colorPattern;
     }
 
     public Integer getColorId() { return this.colorId; }
@@ -55,7 +55,7 @@ public class ThreadColor {
         this.colorFamily = colorFamily;
     }
 
-    public ColorPattern getColorPattern() { return colorPattern; }
-
-    public void setColorPattern(ColorPattern colorPattern) { this.colorPattern = colorPattern; }
+//    public ColorPattern getColorPattern() { return colorPattern; }
+//
+//    public void setColorPattern(ColorPattern colorPattern) { this.colorPattern = colorPattern; }
 }
